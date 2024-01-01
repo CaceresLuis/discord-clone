3de78312a231567d13214086970b7caa0b6d2542
Luis Caceres <56177103+CaceresLuis@users.noreply.github.com>
2024-01-01 14:32:42 -0600

    feat: CR in service of profileModule created (fix channel name in prisma migration) (#6)

--- src/profile/profile.resolver.ts
+++ src/profile/profile.resolver.ts
@@ -1,4 +1,19 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { ProfileService } from './profile.service';
+import { CreateProfileDto } from './profile.dto';
+import { Profile } from './profile.type';
 
 @Resolver()
-export class ProfileResolver {}
+export class ProfileResolver {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @Mutation()
+  async createProfile(@Args('input') input: CreateProfileDto) {
+    return this.profileService.createProfile(input);
+  }
+
+  @Query(() => Profile)
+  async getProfileBiId(@Args('profileId') profileId: number) {
+    return this.profileService.getProfileById(profileId);
+  }
+}
